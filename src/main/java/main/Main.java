c68ec9bc536efab87f5ad70fa358e324674781a9
Häkelmeister <hkelmeister@gmail.com>
2018-03-03 13:14:21 +0100

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+/**
+ * Give user a hit to test this project with JUnit tests.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Run JUnit tests");
+	}
+
+}
---
+++ src/main/java/main/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes, structures ... who does not have it's own package.
+ */
+package main;
